1f8243f9d4dadef567a9e22aa9303b420248b2e3
micheal8100 <99847225+micheal8100@users.noreply.github.com>
2023-06-19 15:09:12 +1200

    startede graph

--- FarmingFeedingAppv2/SheepStats.Designer.cs
+++ FarmingFeedingAppv2/SheepStats.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Collections.Generic;
+
 namespace FarmingFeedingAppv2
 {
     partial class SheepStats
@@ -29,9 +31,17 @@ namespace FarmingFeedingAppv2
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            List<System.Windows.Forms.DataVisualization.Charting.Series> series = new List<System.Windows.Forms.DataVisualization.Charting.Series>();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.lblSheepStats = new System.Windows.Forms.Label();
             this.lblAllSummary = new System.Windows.Forms.Label();
             this.bunHome = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblSheepStats
@@ -49,7 +59,7 @@ namespace FarmingFeedingAppv2
             this.lblAllSummary.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblAllSummary.Location = new System.Drawing.Point(12, 90);
             this.lblAllSummary.Name = "lblAllSummary";
-            this.lblAllSummary.Size = new System.Drawing.Size(581, 351);
+            this.lblAllSummary.Size = new System.Drawing.Size(470, 351);
             this.lblAllSummary.TabIndex = 2;
             // 
             // bunHome
@@ -57,7 +67,7 @@ namespace FarmingFeedingAppv2
             this.bunHome.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.bunHome.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.bunHome.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bunHome.Location = new System.Drawing.Point(599, 367);
+            this.bunHome.Location = new System.Drawing.Point(538, 367);
             this.bunHome.Name = "bunHome";
             this.bunHome.Size = new System.Drawing.Size(189, 71);
             this.bunHome.TabIndex = 5;
@@ -65,17 +75,51 @@ namespace FarmingFeedingAppv2
             this.bunHome.UseVisualStyleBackColor = false;
             this.bunHome.Click += new System.EventHandler(this.bunHome_Click);
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(488, 61);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            title1.Name = "Title";
+            title1.Text = "Sheep breeds";
+            title2.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Left;
+            title2.Name = "food consumed";
+            title2.Text = "food consumed";
+            title3.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            title3.Name = "Breeds";
+            title3.Text = "Breeds";
+            this.chart1.Titles.Add(title1);
+            this.chart1.Titles.Add(title2);
+            this.chart1.Titles.Add(title3);
+            for (int i = 0; i < sm.GetSheepBreeds().Count; i++)
+            {
+                series.Add(new System.Windows.Forms.DataVisualization.Charting.Series());
+                series[series.Count -1].ChartArea = "ChartArea1";
+                series[series.Count -1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                series[series.Count -1].Legend = "Legend1";
+                series[series.Count -1].Name = sm.GetSheepBreeds()[i];
+                this.chart1.Series.Add(series[series.Count - 1]);
+            }
+            // 
             // SheepStats
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.bunHome);
             this.Controls.Add(this.lblAllSummary);
             this.Controls.Add(this.lblSheepStats);
             this.Name = "SheepStats";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SheepStats";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -85,5 +129,6 @@ namespace FarmingFeedingAppv2
         private System.Windows.Forms.Label lblSheepStats;
         private System.Windows.Forms.Label lblAllSummary;
         private System.Windows.Forms.Button bunHome;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
